0f355c49a7c0e68436d52f34ecffa82a73eaa6cd
mtu4554 <mtu4554@gmail.com>
2021-06-03 17:11:38 +0200

    summary working but not pretty

--- src/components/SummaryScreen.js
+++ src/components/SummaryScreen.js
@@ -1,65 +1,113 @@
 import React, { Component } from "react";
 import "../sass/summary.scss";
+import { getDistance } from "geolib";
 
+import Countdown from 'react-countdown';
 class SummaryScreen extends Component {
-
   constructor(props) {
     super(props);
-    this.key = "BrAsr22igPnrx6MHvRmzxbiHIDNfr7hA"
+    this.key = "BrAsr22igPnrx6MHvRmzxbiHIDNfr7hA";
+
     this.state = {
+      timeToArrive: 1627995532*1000,
+      countdownFrom : Date.now(),
       error: null,
       isLoaded: false,
       items: [],
       secondLoaded: false,
       text: "Loading...",
+
     };
+  }
+  componentDidMount() {
+    this.fetchDistanceFromTwoPoints();
     this.fetch();
-    this.fetchDistance();
-    this.fetchPoint();
   }
-  fetchPoint() {
+
+  async fetchDistanceFromTwoPoints() {
+    var first_point = await this.fetchPoint("kosice");
+    first_point = { latitude: first_point.lat, longitude: first_point.lng };
+    var second_point = await this.fetchPoint("bratislava");
+    second_point = { latitude: second_point.lat, longitude: second_point.lng };
+    var distance = getDistance(first_point, second_point) / 1000 + "km";
+    console.log(distance);
+    this.setState({
+      text: distance,
+      secondLoaded: true,
+    });
+    return distance;
+  }
+  timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = a.getMonth();
+    var date = a.getDate();
+    var time =  year + '.' + month + '.'+ date + '.';
+    return time;
+  }
+  async fetchPoint(point) {
     var url =
-      "http://www.mapquestapi.com/geocoding/v1/address?key=BrAsr22igPnrx6MHvRmzxbiHIDNfr7hA&location=Washington,DC";
-
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => console.log(data.results[0].locations[0].displayLatLng));
-  }
-
-  fetchDistance() {
-    fetch(
-      "http://www.mapquestapi.com/geocoding/v1/reverse?key=BrAsr22igPnrx6MHvRmzxbiHIDNfr7hA&location=48.720287208800855, 21.25956559132703&includeRoadMetadata=true&includeNearestIntersection=true"
-    )
+      "http://www.mapquestapi.com/geocoding/v1/address?key=BrAsr22igPnrx6MHvRmzxbiHIDNfr7hA&location=" +
+      point;
+    var return_data = "dull";
+    await fetch(url)
       .then((response) => response.json())
       .then((data) => {
-
-        console.log("dupii")
-        console.log(data.results[0].locations[0].adminArea5)
-        var url =
-        "http://www.mapquestapi.com/directions/v2/route?" +
-        "key=" + this.key + "&" +
-        "from=Bratislava&" +
-        "to=" +data.results[0].locations[0].adminArea5+
-        "unit=k&" +
-        "routeType=fastest&" +
-        "doReverseGeocode=true";
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("dadaaa");
-          console.log(data);
-          this.setState({
-            text: data.toString(),
-            secondLoaded: true,
-          });
-
-        });
-      
-      
-      
+        console.log("point of " + point);
+        return_data = data.results[0].locations[0].displayLatLng;
       });
 
+    return return_data;
   }
+
+  fetchMatrix() {
+    var url =
+      "http://open.mapquestapi.com/directions/v2/routematrix?key=BrAsr22igPnrx6MHvRmzxbiHIDNfr7hA";
+    var json = {
+      locations: ["Kosice Slovakia", "New york"],
+      options: {
+        allToAll: true,
+      },
+    };
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(json),
+    };
+    fetch(url, requestOptions)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+  fetchDistance() {
+    // fetch(
+    //   "http://www.mapquestapi.com/geocoding/v1/reverse?key=BrAsr22igPnrx6MHvRmzxbiHIDNfr7hA&location=48.720287208800855, 21.25956559132703&includeRoadMetadata=true&includeNearestIntersection=true"
+    // )
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    // console.log("dupii")
+    // console.log(data.results[0].locations)
+    //   var url =
+    //   "http://www.mapquestapi.com/directions/v2/route?" +
+    //   "key=" + this.key + "&" +
+    //   "from="+{locations: [{ adminArea1Type: "Country", latLng: {lat: 45.750307,lng: -104.999472}} ]}+
+    //   "to="+{locations: [{ adminArea1Type: "Country", latLng: {lat: 39.750307,lng: -104.999472}} ]}+
+    //   "unit=k&" +
+    //   "routeType=fastest&" +
+    //   "doReverseGeocode=true";
+    // fetch(url)
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log("dadaaa");
+    //     console.log(data);
+    //     this.setState({
+    //       text: data.toString(),
+    //       secondLoaded: true,
+    //     });
+    //   });
+    //});
+  }
+
   fetch() {
     var url = "http://api.open-notify.org/astros.json";
     console.log("fetching...");
@@ -81,7 +129,7 @@ class SummaryScreen extends Component {
       );
   }
   render() {
-    const { error, isLoaded, items, secondLoaded } = this.state;
+    const { error, isLoaded, items, secondLoaded, text,timeToArrive } = this.state;
     console.log(this.state.items);
     if (error) {
       return <div>Error: {error.message}</div>;
@@ -104,9 +152,11 @@ class SummaryScreen extends Component {
       return (
         <div className="spaceContainerSummary">
           <p className="summaryTitle">SUMMMARY</p>
-          <p className="summarySubtitle">TIMELEFT</p>
-          <p className="defaultText">PEOPLE IN LOBBY: {items.number}</p>
-          <p className="defaultText">KM TO TRAVEL: {items.text}</p>
+          <p className="summaryTitle">Time to arrive</p>
+          <Countdown  className="defaultText"  date={timeToArrive} />
+          <p className="defaultText">PEOPLE IN SPACE: {items.number}</p>
+    
+          <p className="defaultText">KM TO TRAVEL: {text}</p>
         </div>
         //   <div className="spaceContainer">
         //      <div className="spaceHeaderContainer">
